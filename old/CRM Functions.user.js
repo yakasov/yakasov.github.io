ccc86509518a584fd6474551981de8a2adcfff08
James McD <yakasovian@gmail.com>
2022-10-20 20:13:19 +0100

    Add Tampermonkey script to be hosted

---
+++ old/CRM Functions.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         CRM Functions
+// @version      1.0
+// @description  Helpful functions for ProspectSoft CRM
+// @author       Yakasov
+// @match        https://crm.prospect365.com/
+// @match        https://crm.prospect365-dev.com/
+// @match        https://crm.prospect365-qa.com/
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
+// ==/UserScript==
+
+const functions = {
+    "Copy environment token": copyToken,
+    "Toggle OData Lite": toggleODataLite,
+    "Run GET request": runGetRequest,
+    "Change UI colour": changeColour,
+};
+
+function copyToken() {
+    navigator.clipboard.writeText(environment.token);
+};
+
+function toggleODataLite() {
+    Api.Query._useODataLite = !Api.Query._useODataLite;
+};
+
+function runGetRequest() {
+    let url = prompt("Enter URL to send GET request to:", "https://api-v1.prospect365-dev.com/Users");
+    $.ajax({
+        url: url,
+        headers: {"authorization":"Bearer " + environment.token, "content-type":"application/json; charset=utf-8","x-core-query":"true","x-locale":"en-gb"},
+        dataType: "json",
+        success: function(response) {
+            GM_openInTab("data:text/json," + encodeURIComponent(JSON.stringify(response, null, "\t")), "_blank");
+        }
+    });
+};
+
+function changeColour() {
+    __test();
+};
+
+for (const [key, val] of Object.entries(functions)) {
+    GM_registerMenuCommand(key, val);
+};
+
