e7fe59f45edcc0172e11e0995f3a2380a84d3a77
yakasov <yakasovian@gmail.com>
2024-03-10 01:04:03 +0000

    add people!! no content yet

---
+++ pages/loader.js
@@ -0,0 +1,23 @@
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    loadContent(); // Load dynamic content
+  });
+
+  function loadContent() {
+    const innerContent = document.getElementsByTagName("body")[0].innerHTML;
+
+    fetch("/pages/template.html")
+      .then((response) => response.text())
+      .then((outerHTML) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(outerHTML, "text/html");
+
+        document.body.innerHTML = doc.body.innerHTML;
+
+        document.getElementById("main").innerHTML = innerContent;
+      })
+      .catch((error) => {
+        console.error("Failed to load outer template", error);
+      });
+  }
+})();
