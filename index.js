db7cb3cd73e054f21c54656b1d88aaf3192091a5
yakasov <yakasovian@gmail.com>
2025-05-26 21:13:29 +0100

    WIP New homepage

---
+++ index.js
@@ -0,0 +1,14 @@
+function updateMain(sectionTitle) {
+  let titleEl = document.getElementById("main-title");
+  let allSections = document.querySelectorAll("#main div");
+  let sectionEl = document.getElementById(`main-${sectionTitle}`);
+
+  titleEl.innerText =
+    sectionTitle.charAt(0).toUpperCase() + sectionTitle.slice(1);
+  allSections.forEach((s) => {
+    if (!s.classList.contains("hidden")) {
+      s.classList.add("hidden");
+    }
+  });
+  sectionEl.classList.remove("hidden");
+}
